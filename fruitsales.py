fc91a84e874c05391c9da199b6a8da6cab17ec4e
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-08-31 20:27:33 -0400

    test2

--- fruitsales.py
+++ fruitsales.py
@@ -6,4 +6,4 @@ fruit_sales = pd.DataFrame({'Apples':[35, 41],
              index=['2017 Sales', '2018 Sales'])
 
 
-fruit_sales.to_csv('fruit.csv')
+fruit_sales.to_csv('fruit.csv', header=False, index=False)
