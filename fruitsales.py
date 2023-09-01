978782321b6db4228642d247793400cf54fb5283
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-08-31 20:44:05 -0400

    test4

--- fruitsales.py
+++ fruitsales.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-fruit_sales = pd.DataFrame({'Apples':[35,41], 
-             'Bannanas':[21,34]}, 
+df = pd.DataFrame({'Apples':[35, 41], 
+             'Bananas':[21, 34]}, 
              index=['2017 Sales','2018 Sales'])
 
-print(fruit_sales)
-fruit_sales.to_csv('fruit.csv')
+print(df)
+df.to_csv('fruit.csv')
